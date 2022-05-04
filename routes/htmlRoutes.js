9b250182bb36bca0b9585522db1593bfa2822d37
Dawson Filion <Dawsonfilion21@gmail.com>
2022-05-04 13:56:40 -0400

    GET req for notes.html

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,10 @@
+const path = require("path");
+const router = require("express").Router();
+
+//HTML GET Requests
+//code below is for when a user visits a page 
+
+router.get("notes/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+
+});
