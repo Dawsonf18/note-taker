51d576814c5dc00e8ee4606473da2940e972a941
Dawson Filion <Dawsonfilion21@gmail.com>
2022-05-04 14:02:02 -0400

    GET req for index.html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -8,3 +8,10 @@ router.get("notes/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 
 });
+
+router.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+
+});
+
+module.exports = router;
