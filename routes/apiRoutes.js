7234e368e4bf8026331f8554d95132600d20ed3e
Dawson Filion <Dawsonfilion21@gmail.com>
2022-05-03 21:54:29 -0400

    router.post

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -18,3 +18,10 @@ router.get("/notes", function (req, res) {
         .then(notes => res.json(notes))
         .catch(err => res.status(500).json(err))
 });
+
+router.post("/notes", function (req, res) {
+    store
+        .addNote(req.body)
+        .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err))
+});
